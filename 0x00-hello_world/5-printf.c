eb81579952b6a336454cdf745d8c8c05781b0cb8
Ezechuks <kosyezechuks@gmail.com>
2022-09-08 13:51:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <studio.h>
 
 /**
- * main - Print a text
+ * main - Prints a text
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
