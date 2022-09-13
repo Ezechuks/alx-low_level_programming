e2cf8bf617e57a88cccca75a40db367b9c8b4e0f
Ezechuks <kosyezechuks@gmail.com>
2022-09-13 16:10:20 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers froom input 98,
+ * print_to_98 - Prints all natural numbers from input 98,
  * @n: The number to begin counting at.
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 		while (n > 98)
 			printf("%d\n", n);
