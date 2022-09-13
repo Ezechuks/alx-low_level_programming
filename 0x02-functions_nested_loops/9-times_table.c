19bbf0909debdbf32a4e0896db44660faf36dec5
Ezechuks <kosyezechuks@gmail.com>
2022-09-13 15:58:01 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
-		{			
+		{		
 			_putchar(',');
 			_putchar(' ');
 
@@ -23,7 +23,7 @@ void times_table(void)
 
 			_putchar((prod % 10) + '0');
 		}
-	putchar('\n');
+	_putchar('\n');
 	}
 }
 
