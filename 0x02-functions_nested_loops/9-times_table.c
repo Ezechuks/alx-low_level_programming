cf75d4e94d99e3618eec47ba2136bc16d325110a
Ezechuks <kosyezechuks@gmail.com>
2022-09-13 16:32:34 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * times_table - prints the 9 times-table, starting with 0
+ * times_table - Prints the 9 times table, starting with 0,
  */
 void times_table(void)
 {
@@ -11,7 +12,7 @@ void times_table(void)
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
-		{		
+		{
 			_putchar(',');
 			_putchar(' ');
 
@@ -20,10 +21,8 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-
 			_putchar((prod % 10) + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
-
