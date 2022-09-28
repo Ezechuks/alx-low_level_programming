61f244e33258b9547c89ffa3da6e885572095b5c
Ezechuks <kosyezechuks@gmail.com>
2022-09-28 14:07:00 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@ int _sqrt(int, int);
 /**
  * _sqrt_recursion - sqrt using recursion
  * @n: parameter to be sqrt
- * 
+ *
  * Return: square root of a number
  */
 
@@ -32,3 +32,4 @@ int _sqrt(int n, int i)
 	if (square == n)
 		return (i);
 	return (_sqrt(n, i + 1));
+}
