e9d6534279d4af2b4d241fbe1c06d40976b38ac6
Ezechuks <kosyezechuks@gmail.com>
2022-09-30 16:30:18 -0700

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,17 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0, num1, num2;
+	int result = 0, num, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
-
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
 	}
 
@@ -31,4 +31,5 @@ int main(int argc, char *argv[])
 		result += num;
 	}
 	printf("%d\n", result);
-	return (0)
+	return (0);
+}
