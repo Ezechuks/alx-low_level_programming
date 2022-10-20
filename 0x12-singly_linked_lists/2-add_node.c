3d2da901138ad979335c843905c874cf0bf4bda0
Ezechuks <kosyezechuks@gmail.com>
2022-10-20 11:35:19 -0700

    commiting

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_note - adds a new mode at the beginning of a list
+ * @head: head of linked list
+ * @str: string to store in the list
+ *
+ * Return: address of the head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	size_t n;
+
+	new_mode = malloc(sizeof(list_t));
+	if (new_mode == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++);
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
