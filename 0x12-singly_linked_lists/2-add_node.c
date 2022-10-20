e6f841e00f15bcf21c337d38f5fb4ee6ac4f07cd
Ezechuks <kosyezechuks@gmail.com>
2022-10-20 11:36:44 -0700

    commiting

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,13 +13,14 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	size_t n;
 
-	new_mode = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_mode == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
 
-	for (n = 0; str[n]; n++);
+	for (n = 0; str[n]; n++)
+		;
 
 	new_node->len = n;
 	new_node->next = *head;
